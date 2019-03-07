18140e05d0083bd94a806f19b0efb6681195055d
Luis <villazonpersonal@gmail.com>
2019-03-07 16:37:33 +0100

    Convert string to Integers

--- src/main/java/org/uda/techfest/numbers/SumNumbers.java
+++ src/main/java/org/uda/techfest/numbers/SumNumbers.java
@@ -2,6 +2,8 @@ package org.uda.techfest.numbers;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 
 public class SumNumbers {
@@ -9,6 +11,8 @@ public class SumNumbers {
     static void SumNumbers(){
         Pipeline pipeline = Pipeline.create();
         pipeline.apply("Read File", TextIO.read().from("/files/techfest/numbers.txt"))
+                .apply("Convert Strings to Integer", MapElements.into(TypeDescriptors.integers()).via((String s)-> Integer.valueOf(s)))
+                .apply("Convert Integer to String", MapElements.into(TypeDescriptors.strings()).via((Integer i) -> String.valueOf(i)))
                 .apply("Write output", TextIO.write().to("exit"));
         pipeline.run().waitUntilFinish();
     }
