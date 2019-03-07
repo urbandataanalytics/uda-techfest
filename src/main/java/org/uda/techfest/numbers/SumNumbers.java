ae505e99578a3642700e1603b70efc3f7443c7ac
Luis <villazonpersonal@gmail.com>
2019-03-07 16:49:56 +0100

    Count prime numbers

--- src/main/java/org/uda/techfest/numbers/SumNumbers.java
+++ src/main/java/org/uda/techfest/numbers/SumNumbers.java
@@ -2,6 +2,7 @@ package org.uda.techfest.numbers;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.TypeDescriptors;
@@ -27,7 +28,8 @@ public class SumNumbers {
         pipeline.apply("Read File", TextIO.read().from("/files/techfest/numbers.txt"))
                 .apply("Convert Strings to Integer", MapElements.into(TypeDescriptors.integers()).via((String s)-> Integer.valueOf(s)))
                 .apply("Filter prime numbers", Filter.by((Integer i) -> isPrime(i)))
-                .apply("Convert Integer to String", MapElements.into(TypeDescriptors.strings()).via((Integer i) -> String.valueOf(i)))
+                .apply(Count.globally())
+                .apply("Convert Long to String", MapElements.into(TypeDescriptors.strings()).via((Long i) -> String.valueOf(i)))
                 .apply("Write output", TextIO.write().to("exit"));
         pipeline.run().waitUntilFinish();
     }
